c498ce56ed1c80f2bce513502b2731e0f9dee717
John Carlson <yottzumm@gmail.com>
2017-04-06 02:47:11 -0400

    First checkin

---
+++ validate.js
@@ -0,0 +1,59 @@
+var fs = require('fs');
+let jsonlint = require('jsonlint');
+
+var validate = {};
+
+var Ajv = require('ajv');
+
+function setVersion(version) {
+	var versions = { "3.0":true,"3.1":true,"3.2":true,"3.3":true,"3.4":true, "4.0":true }
+	if (!versions[version]) {
+		console.error("Can only validate version 3.0-4.0 presently. Switching version to 3.3.");
+		version = "3.3";
+	}
+	if (!validate[version]) {
+		var schema = fs.readFileSync("x3d-"+version+"-JSONSchema.json").toString();
+		var ajv = new Ajv({ allErrors:true});
+		ajv.addFormat("uri", /^(?:[a-z][a-z0-9+\-.]*:)?(?:\/?\/(?:(?:[a-z0-9\-._~!$&'()*+,;=:]|%[0-9a-f]{2})*@)?(?:\[(?:(?:(?:(?:[0-9a-f]{1,4}:){6}|::(?:[0-9a-f]{1,4}:){5}|(?:[0-9a-f]{1,4})?::(?:[0-9a-f]{1,4}:){4}|(?:(?:[0-9a-f]{1,4}:){0,1}[0-9a-f]{1,4})?::(?:[0-9a-f]{1,4}:){3}|(?:(?:[0-9a-f]{1,4}:){0,2}[0-9a-f]{1,4})?::(?:[0-9a-f]{1,4}:){2}|(?:(?:[0-9a-f]{1,4}:){0,3}[0-9a-f]{1,4})?::[0-9a-f]{1,4}:|(?:(?:[0-9a-f]{1,4}:){0,4}[0-9a-f]{1,4})?::)(?:[0-9a-f]{1,4}:[0-9a-f]{1,4}|(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?))|(?:(?:[0-9a-f]{1,4}:){0,5}[0-9a-f]{1,4})?::[0-9a-f]{1,4}|(?:(?:[0-9a-f]{1,4}:){0,6}[0-9a-f]{1,4})?::)|[Vv][0-9a-f]+\.[a-z0-9\-._~!$&'()*+,;=:]+)\]|(?:(?:25[0-5]|2[0-4]\d|[01]?\d\d?)\.){3}(?:25[0-5]|2[0-4]\d|[01]?\d\d?)|(?:[a-z0-9\-._~!$&'()*+,;=]|%[0-9a-f]{2})*)(?::\d*)?(?:\/(?:[a-z0-9\-._~!$&'()*+,;=:@]|%[0-9a-f]{2})*)*|\/(?:(?:[a-z0-9\-._~!$&'()*+,;=:@]|%[0-9a-f]{2})+(?:\/(?:[a-z0-9\-._~!$&'()*+,;=:@]|%[0-9a-f]{2})*)*)?|(?:[a-z0-9\-._~!$&'()*+,;=:@]|%[0-9a-f]{2})+(?:\/(?:[a-z0-9\-._~!$&'()*+,;=:@]|%[0-9a-f]{2})*)*)?(?:\?(?:[a-z0-9\-._~!$&'()*+,;=:@\/?]|%[0-9a-f]{2})*)?(?:\#(?:[a-z0-9\-._~!$&'()*+,;=:@\/?]|%[0-9a-f]{2})*)?$/i);
+		validate[version] = ajv.compile(JSON.parse(schema));
+	}
+	return version;
+}
+
+function validateJSON() {
+
+	var files = process.argv;
+	for (var f in files) {
+		var file = files[f];
+		try {  
+			var str = fs.readFileSync(file).toString();
+			if (typeof str === 'undefined') {
+				throw("Read nothing, or possbile error");
+			}
+			var json = jsonlint.parse(str);
+			var version = json.X3D["@version"];
+			version = setVersion(version);  // loads schema.
+			var valid = validate[version](json);
+			if (!valid) {
+				var errs = validate[version].errors;
+				var error = ""
+				for (var e in errs) {
+					error += "\r\n dataPath: " + errs[e].dataPath + "\r\n";
+					error += " message: " + errs[e].message + "\r\n";
+					error += " params: " + JSON.stringify(errs[e].params) + "\r\n";
+				}
+				throw error;
+			}
+				
+		} catch (e) {
+			console.error("================================================================================");
+			console.error(file);
+			console.error("Error reading", file, e);
+		}
+	}
+}
+
+process.argv.shift();
+process.argv.shift();
+
+validateJSON();
