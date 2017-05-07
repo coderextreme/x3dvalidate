43a8c53b032a406b624966ecff2d3ab4bf00ee96
John Carlson <yottzumm@gmail.com>
2017-05-07 06:45:02 -0400

    See if you like this better.

--- index.js
+++ index.js
@@ -79,7 +79,7 @@ function doValidate(json, file) {
 			for (var e in errs) {
 				error += "\r\n keyword: " + errs[e].keyword + "\r\n";
 	
-				error += " dataPath: " + (errs[e].dataPath.replace(/[\.\[\]']+/g, "/")) + "\r\n";
+				error += " dataPath: " + (errs[e].dataPath.replace(/[\.\[\]']+/g, " > ")) + "\r\n";
 				error += " message: " + errs[e].message + "\r\n";
 				error += " params: " + JSON.stringify(errs[e].params) + "\r\n";
 				error += " file: " + file + "\r\n";
