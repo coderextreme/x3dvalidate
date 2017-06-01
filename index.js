4672a5893de9861a4825585bc93498f84b3bff04
Axel Korn <git@alexkern.de>
2017-06-01 18:15:05 +0200

    built ajv-i18n into, yet without variable to set language

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ var fs = require('fs');
 var http = require('http');
 var jsonlint = require('jsonlint');
 var selectObjectFromJson = require('./selectObjectFromJson');
-
+var localize = require('ajv-i18n');
 
 var validate = function() { return false; }
 
@@ -15,6 +15,7 @@ function doValidate(json, validated_version, file, success, failure) {
 	if (typeof validated_version !== 'undefined') {
 		var valid = validated_version(json);
 		if (!valid) {
+			localize.de(validated_version.errors);
 			console.error("================================================================================");
 			console.error("File:", file);
 			var errs = validated_version.errors;
