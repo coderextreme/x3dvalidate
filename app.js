1ddc27b4845c6f58a32543b60230eeb54716392f
John Carlson <yottzumm@gmail.com>
2023-06-09 19:26:21 -0500

    Updated

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 var validate = require('./x3dvalidate')
 
-var files = [ "ball.json", "BasicPointSprites1.json", "BasicPointSprites2.json", "HAnimModelFootLeft.json", "HAnimModelFootRight.json", "HAnimModelHandLeft.json", "HAnimModelHandRight.json", "HAnimModelsHandsFeet.json", "HelloWorldProgramOutput.json", "KoreanCharacterMotionAnnexC01Jin.json", "KoreanCharacterMotionAnnexD01Jin.json", "flipper.json"];
+var files = [ "ball.json", "BasicPointSprites1.json", "BasicPointSprites2.json", "HAnimModelFootLeft.json", "HAnimModelFootRight.json", "HAnimModelHandLeft.json", "HAnimModelHandRight.json", "HAnimModelsHandsFeet.json", "HelloWorldProgramOutput.json", "KoreanCharacterMotionAnnexC01Jin.json", "KoreanCharacterMotionAnnexD01Jin.json", "flipper.json", 
+"VolumeEmitter.json", "BoundedPhysicsModel.json" ];
 
 validate("en", files);
