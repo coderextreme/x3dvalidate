04c72a6967725abf2ba2e2967c21fbfbd12c132c
John Carlson <yottzumm@gmail.com>
2023-06-30 00:32:29 -0500

    Updates, cleaned up JSON files

--- app.js
+++ app.js
@@ -1,6 +1,5 @@
 var validate = require('./x3dvalidate')
 
-var files = [ "ball.json", "BasicPointSprites1.json", "BasicPointSprites2.json", "HAnimModelFootLeft.json", "HAnimModelFootRight.json", "HAnimModelHandLeft.json", "HAnimModelHandRight.json", "HAnimModelsHandsFeet.json", "HelloWorldProgramOutput.json", "KoreanCharacterMotionAnnexC01Jin.json", "KoreanCharacterMotionAnnexD01Jin.json", "flipper.json", 
-"VolumeEmitter.json", "BoundedPhysicsModel.json" ];
+var files = [ "HelloWorldProgramOutput.json", "HelloWorldProgramOutput.json.corrected" ];
 
 validate("en", files);
