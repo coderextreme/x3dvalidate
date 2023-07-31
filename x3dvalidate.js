f46760737975ccbae53eb622dcbfd434babeface
John Carlson <yottzumm@gmail.com>
2023-07-30 20:41:07 -0500

    Added message.

--- x3dvalidate.js
+++ x3dvalidate.js
@@ -1,11 +1,13 @@
 #!/usr/bin/env node
 "use strict"
-
 var validate = require('./src/index.js')
 
 process.argv.shift();
 process.argv.shift();
 var files = process.argv;
+if (files.length === 0) {
+	console.log("I see", files.length, "parameters.  Is x3dvalidate installed properly?");
+}
 
 validate(files);
 
